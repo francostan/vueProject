e478c39a500b68c38c44e77957b43cc7ab16838f
francostan98 <francostan98@gmail.com>
2023-12-01 19:51:23 +0000

    falta reactividad

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-const name = 'Franui'
+let name = 'Franui'
+
+const handleClick = () => {
+ console.log('click')
+}
 </script>
 
 <template>
  <h1 class="text-3xl font-bold underline">
     Hello {{ name }}
   </h1>
+  <button @click=handleClick>Cambiar nombre</button>
 </template>
 
 <style scoped>
